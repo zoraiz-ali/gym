ea7c54c476be8ea691dd1d6ca6800d4b0c6fcc9d
zoraiz-ali <74529707+zoraiz-ali@users.noreply.github.com>
2020-12-03 14:25:55 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+
+setup(name="gym_robot",
+      version="0.1",
+      url="https://github.com/zoraiz-ali/gym.git",
+      author="Zoraiz Ali",
+      license="MIT",
+      packages=["gym_robot", "gym_robot.envs"],
+      
+      install_requires = ["gym", "numpy"]
+)
+
