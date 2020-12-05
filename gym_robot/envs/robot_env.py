f0f8cf89bd1e2721f9aaf3aec1f1df1155259ff3
zoraiz-ali <74529707+zoraiz-ali@users.noreply.github.com>
2020-12-05 11:59:27 +0500

    Add files via upload

---
+++ gym_robot/envs/robot_env.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+from gym_robot.envs.gym_robot2d import Environment
+
+
+class RobotEnv(gym.Env):
+    metadata = {
+        "render.modes": ["human", "rgb_array"],
+    }
+    
+    def __init__(self):
+        self.environment = Environment()
+        self.action_space = spaces.Discrete(self.environment.n_actions)
+        low = np.zeros(self.environment.reset(), dtype=float)
+        high = np.ones(self.environment.reset(), dtype=float)
+        self.observation_space = spaces.Box(low, high, dtype=np.float)
+        
+        
+    def reset(self):
+        del self.environment
+        self.environmet = Environment()
+        obs = self.environment.reset()
+        return obs  
+    
+    
+    def step(self, action):
+        self.environment.action(action)       
+        return  next_state, reward, done
+    
+    def render(self, mode="human", close=False):
+        self.environment.render()
+    
+
