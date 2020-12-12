fc2be1419f3e4538ec7a8c5ff3f9e212cd219357
Thymen Rijpkema <t.d.j.rijpkema@gmail.com>
2020-12-12 14:29:24 +0100

    Updated README and setup

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 
 from setuptools import setup
 
-setup(name="gym_robot",
-      version="0.2",
-      url="https://github.com/zoraiz-ali/gym.git",
-      author="Zoraiz Ali",
-      license="MIT",
-      packages=["gym_robot", "gym_robot.envs"],
-      
-      install_requires = ["gym", "numpy"]
-)
+setup(
+        name="gym_robot",
+        version="0.3",
+        url="https://github.com/zoraiz-ali/gym.git",
+        author="Zoraiz Ali, T.D.J. Rijpkema",
+        license="MIT",
+        packages=["gym_robot", "gym_robot.envs"],
 
+        install_requires=["gym", "numpy", "opencv-python"]
+)
