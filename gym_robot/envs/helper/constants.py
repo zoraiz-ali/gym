559a7c75a18a521d4ead6252c9b0aad76c42a33d
zoraiz-ali <74529707+zoraiz-ali@users.noreply.github.com>
2021-01-17 11:00:13 +0500

    Update constants.py

--- gym_robot/envs/helper/constants.py
+++ gym_robot/envs/helper/constants.py
@@ -7,13 +7,13 @@ SIZE_SQUARE = 40
 FPS = 30
 
 MAP: Dict[Tuple[int, int], str] = {
-    (0, 0): 'agent_1', (0, 1): 'black', (0, 5): 'black', (0, 8): 'black',
-    (1, 7): 'black',
-    (2, 0): 'black', (2, 3): 'black',
-    (3, 2): 'black', (3, 5): 'black', (3, 7): 'black', (3, 8): 'black',
-    (4, 0): 'black', (4, 3): 'black',
-    (5, 5): 'black', (5, 6): 'black', (5, 7): 'black',
-    (6, 1): 'black', (6, 5): 'black', (6, 6): 'flag', (6, 7): 'black',
-    (7, 3): 'black', (7, 5): 'black', (7, 7): 'black',
-    (8, 0): 'black',
+    (0, 0): 'agent_1', (0, 1): 'road_closed_1', (0, 5): 'road_closed_1', (0, 8): 'road_closed_1',
+    (1, 7): 'road_closed_1',
+    (2, 0): 'road_closed_1', (2, 3): 'road_closed_1',
+    (3, 2): 'road_closed_1', (3, 5): 'road_closed_1', (3, 7): 'road_closed_1', (3, 8): 'road_closed_1',
+    (4, 0): 'road_closed_1', (4, 3): 'road_closed_1',
+    (5, 5): 'road_closed_1', (5, 6): 'road_closed_1', (5, 7): 'road_closed_1',
+    (6, 1): 'road_closed_1', (6, 5): 'road_closed_1', (6, 6): 'flag', (6, 7): 'road_closed_1',
+    (7, 3): 'road_closed_1', (7, 5): 'road_closed_1', (7, 7): 'road_closed_1',
+    (8, 0): 'road_closed_1',
 }
