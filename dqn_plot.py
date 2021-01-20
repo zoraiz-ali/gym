b0fc1f58100f8ed59c3b9bc68b0e4fefd17752bb
zoraiz-ali <74529707+zoraiz-ali@users.noreply.github.com>
2021-01-20 12:42:07 +0500

    Update dqn_plot.py

--- dqn_plot.py
+++ dqn_plot.py
@@ -85,7 +85,7 @@ class DQNAgent:
                   
                     #
                     plt.figure()
-                    plt.plot(np.arange(steps), steps, 'b')
+                    plt.plot(np.arange(len(steps_per_episode)),steps_per_episode, 'b')
                     plt.title('Episode via steps')
                     plt.xlabel('EPISODES')
                     plt.ylabel('steps')      
@@ -101,91 +101,35 @@ if __name__ == "__main__":
     #agent.model.summary()
     done = False
     batch_size = 32
-
-    for e in range(EPISODES):
+    steps_per_episode = []
+    for episode in range(10):
         state = env.reset()
         state = np.reshape(state, [1, 9,9])
         done = False
         score = 0
-        steps = 0
+        i = 0
         cost = 0
-        while not done:
+        while not done :
             env.render()
             action = agent.act(state)
             next_state, reward, done, _ = env.step(action)
-           
+            i += 1
             if not done: 
                 reward = reward
             score += reward
-            steps += 1
-            
+            steps_per_episode = [i]
+                
             next_state = np.reshape(next_state, [1, 9,9])
             agent.memorize(state, action, reward, next_state, done)
             state = next_state
             if done:
-                print("episode: {}/{}, step: {}, e: {:.2}"
-                      .format(e, EPISODES, steps, agent.epsilon))
+                
+                print("episode: {}, step: {}, e: {:.2}"
+                      .format(episode, steps_per_episode,agent.epsilon))
                 
                 
                 break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
-    agent.plot_results( steps)
-    
-    
-    #Output
- episode: 0/10, step: 22, e: 1.0
-episode: 1/10, step: 1, e: 1.0
-episode: 2/10, step: 5, e: 1.0
-episode: 3/10, step: 12, e: 0.97
-episode: 4/10, step: 4, e: 0.95
-episode: 5/10, step: 13, e: 0.9
-episode: 6/10, step: 3, e: 0.89
-episode: 7/10, step: 2, e: 0.88
-episode: 8/10, step: 7, e: 0.86
-episode: 9/10, step: 3, e: 0.85
----------------------------------------------------------------------------
-ValueError                                Traceback (most recent call last)
-<ipython-input-1-3c74082a72ba> in <module>
-    131             if len(agent.memory) > batch_size:
-    132                 agent.replay(batch_size)
---> 133     agent.plot_results( steps)
-    134 
-
-<ipython-input-1-3c74082a72ba> in plot_results(self, steps)
-     86                     #
-     87                     plt.figure()
----> 88                     plt.plot(np.arange(steps), steps, 'b')
-     89                     plt.title('Episode via steps')
-     90                     plt.xlabel('EPISODES')
-
-~\anaconda3\lib\site-packages\matplotlib\pyplot.py in plot(scalex, scaley, data, *args, **kwargs)
-   2759 @docstring.copy(Axes.plot)
-   2760 def plot(*args, scalex=True, scaley=True, data=None, **kwargs):
--> 2761     return gca().plot(
-   2762         *args, scalex=scalex, scaley=scaley, **({"data": data} if data
-   2763         is not None else {}), **kwargs)
-
-~\anaconda3\lib\site-packages\matplotlib\axes\_axes.py in plot(self, scalex, scaley, data, *args, **kwargs)
-   1645         """
-   1646         kwargs = cbook.normalize_kwargs(kwargs, mlines.Line2D)
--> 1647         lines = [*self._get_lines(*args, data=data, **kwargs)]
-   1648         for line in lines:
-   1649             self.add_line(line)
-
-~\anaconda3\lib\site-packages\matplotlib\axes\_base.py in __call__(self, *args, **kwargs)
-    214                 this += args[0],
-    215                 args = args[1:]
---> 216             yield from self._plot_args(this, kwargs)
-    217 
-    218     def get_next_color(self):
-
-~\anaconda3\lib\site-packages\matplotlib\axes\_base.py in _plot_args(self, tup, kwargs)
-    340 
-    341         if x.shape[0] != y.shape[0]:
---> 342             raise ValueError(f"x and y must have same first dimension, but "
-    343                              f"have shapes {x.shape} and {y.shape}")
-    344         if x.ndim > 2 or y.ndim > 2:
-
-ValueError: x and y must have same first dimension, but have shapes (3,) and (1,)
+agent.plot_results( steps_per_episode)     
 
