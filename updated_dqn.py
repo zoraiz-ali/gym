2260bebe3d418d7e79bb195c3524bdef32c2762f
zoraiz-ali <74529707+zoraiz-ali@users.noreply.github.com>
2021-02-02 12:26:37 +0500

    Update updated_dqn.py

--- updated_dqn.py
+++ updated_dqn.py
@@ -4,6 +4,7 @@ import gym_robot
 import gym_robot.envs
 import gym_robot.envs.helper
 import numpy as np
+import pyglet
 from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
@@ -23,13 +24,13 @@ class DQNAgent:
         self.action_size = action_size
    
    #we make memory to train our agent
-        self.memory = deque(maxlen = 200000)
+        self.memory = deque(maxlen = 20000)
 
         self.gamma = 0.95
         self.epsilon = 0.9
         self.epsilon_decay = 0.995
         self.epsilon_min = 0.01
-        self.learning_rate= 0.5
+        self.learning_rate= 0.001
         self.model = self._build_model()
         #self.model = OurModel(input_shape=(self.state_size), action_space = self.action_size)
     
@@ -71,8 +72,8 @@ class DQNAgent:
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
-        #if self.epsilon > self.epsilon_min:
-            #self.epsilon *= self.epsilon_decay
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
     
     def load (self, name):
         self.model.load_weights(name)
@@ -81,14 +82,20 @@ class DQNAgent:
         self.model.save_weights(name)    
         
     # Plotting the results for the number of steps
-    def plot_results(self, steps):
+    def plot_results(self, steps, score):
                   
                     #
                     plt.figure()
-                    plt.plot(np.arange(len(steps_per_episode)),steps_per_episode, 'b')
+                    plt.plot(np.arange(len(steps_per_episode)),steps_per_episode )
                     plt.title('Episode via steps')
                     plt.xlabel('EPISODES')
-                    plt.ylabel('steps')      
+                    plt.ylabel('steps')
+                    
+                    plt.figure()
+                    plt.plot(np.arange(len(score_per_episode)),score_per_episode )
+                    plt.title('Episode via score')
+                    plt.xlabel('EPISODES')
+                    plt.ylabel('score')
 
                    # Showing the plots
                     plt.show()
@@ -102,21 +109,23 @@ if __name__ == "__main__":
     done = False
     batch_size = 32
     steps_per_episode = []
-    for episode in range(1001):
+    score_per_episode = []
+    for episode in range(1000):
         state = env.reset()
         state = np.reshape(state, [1, 9,9])
         done = False
         score = 0
         i = 0
-        cost = 0
+        
         while not done :
             env.render()
             action = agent.act(state)
             next_state, reward, done, _ = env.step(action)
             i += 1
+            score +=reward
             if not done: 
                 reward = reward
-            score += reward
+            
             
                 
             next_state = np.reshape(next_state, [1, 9,9])
@@ -124,12 +133,13 @@ if __name__ == "__main__":
             state = next_state
             if done:
                 steps_per_episode.append(i)
-                print("episode: {},step:{}, step: {}, e: {:.2}"
-                      .format(episode, i, steps_per_episode,agent.epsilon))
+                score_per_episode.append(score)
+                print("episode: {},step:{}, step: {}, score: {},  e: {:.2}"
+                      .format(episode, i, steps_per_episode, score,   agent.epsilon))
                 
                 
                 break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
-agent.plot_results( steps_per_episode)          
+agent.plot_results( steps_per_episode, score_per_episode)          
        
