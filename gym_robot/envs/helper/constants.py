62f856ccb2ebb396cb4d6c3181b10bf2f861ea5f
zoraiz-ali <74529707+zoraiz-ali@users.noreply.github.com>
2021-02-15 12:19:32 +0500

    Update constants.py

--- gym_robot/envs/helper/constants.py
+++ gym_robot/envs/helper/constants.py
@@ -7,13 +7,13 @@ SIZE_SQUARE = 40
 FPS = 30
 
 MAP: Dict[Tuple[int, int], str] = {
-    (0, 0): 'agent_1', (0, 1): 'road_closed_1', (0, 5): 'road_closed_1', (0, 8): 'road_closed_1',
-    (1, 7): 'road_closed_1',
-    (2, 0): 'road_closed_1', (2, 3): 'road_closed_1',
-    (3, 2): 'road_closed_1', (3, 5): 'road_closed_1', (3, 7): 'road_closed_1', (3, 8): 'road_closed_1',
-    (4, 0): 'road_closed_1', (4, 3): 'road_closed_1',
-    (5, 5): 'road_closed_1', (5, 6): 'road_closed_1', (5, 7): 'road_closed_1',
-    (6, 1): 'road_closed_1', (6, 5): 'road_closed_1', (6, 6): 'flag', (6, 7): 'road_closed_1',
-    (7, 3): 'road_closed_1', (7, 5): 'road_closed_1', (7, 7): 'road_closed_1',
-    (8, 0): 'road_closed_1',
+    (0, 0): 'agent_1', (0, 1): 'building_2', (0, 5): 'bank_2', (0, 8): 'tree_2',
+    (1, 7): 'shop',
+    (2, 0): 'road_closed_2', (2, 3): 'tree_1',
+    (3, 2): 'tree_1', (3, 5): 'road_closed_3', (3, 7): 'pedestrian', (3, 8): 'traffic_lights',
+    (4, 0): 'road_closed_1', (4, 3): 'tree_1',
+    (5, 5): 'building_1', (5, 6): 'building_1', (5, 7): 'building_1',
+    (6, 1): 'bank_1', (6, 5): 'building_1', (6, 6): 'flag', (6, 7): 'building_1',
+    (7, 3): 'traffic_lights', (7, 5): 'building_1', (7, 7): 'building_1',
+    (8, 0): 'tree_2',
 }
