6f1cdf486206c6f007a06101bc8889ece58aab45
zoraiz-ali <74529707+zoraiz-ali@users.noreply.github.com>
2021-03-15 00:55:31 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         name="gym_robot",
         version="0.3",
         url="https://github.com/zoraiz-ali/gym.git",
-        author="Zoraiz Ali, T.D.J. Rijpkema",
+        author="Zoraiz Ali",
         license="MIT",
         packages=["gym_robot", "gym_robot.envs", "gym_robot.envs.helper", ],
         include_package_data=True,
