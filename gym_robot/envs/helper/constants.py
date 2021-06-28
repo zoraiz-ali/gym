0d53c3c111f0bc95e2e3c8b7d0ee32b3c306f6c1
zoraiz-ali <74529707+zoraiz-ali@users.noreply.github.com>
2021-06-28 13:10:18 +0500

    Update constants.py

--- gym_robot/envs/helper/constants.py
+++ gym_robot/envs/helper/constants.py
@@ -11,8 +11,9 @@ MAP: Dict[Tuple[int, int], str] = {
     (1, 3): 'black', (1, 4): 'black', (1, 5): 'black', (1, 6): 'black', 
     (3, 7): 'black',
     (4, 4): 'black',
-    (5,0): 'black', (5,2): 'black', (5,4): 'black',
-    (7, 3): 'black', (7, 4): 'black', (7, 5): 'black', (7, 6): 'black', (7, 7): 'black',
+    (5,0): 'black', (5,4): 'black',
+    (6,3): 'black',
+    (9, 3): 'black', (9, 4): 'black', (9, 5): 'black', (9, 6): 'black', 
     (10,10): 'flag',
     
 }
